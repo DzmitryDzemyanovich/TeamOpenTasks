6ef033fd848277ff82c59d1e946081fd84ae1665
unknown <kelus@tut.by>
2019-03-18 21:45:11 +0100

    feat: webapp

---
+++ TeamOpenTasks.WebApi/Controllers/ValuesController.fs
@@ -0,0 +1,36 @@
+﻿namespace TeamOpenTasks.WebApi.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Mvc
+open TeamOpenTasks.TestData
+open TeamOpenTasks.Teams
+
+[<Route("api/[controller]")>]
+[<ApiController>]
+type ValuesController () =
+    inherit ControllerBase()
+
+    [<HttpGet>]
+    member this.Get() =
+        let values = Teams.allTeams //[|"value1"; "value2"|]
+        ActionResult<Team[]>(values |> List.toArray)
+
+    [<HttpGet("{id}")>]
+    member this.Get(id:int) =
+        let value = "value"
+        ActionResult<string>(value)
+
+    [<HttpPost>]
+    member this.Post([<FromBody>] value:string) =
+        ()
+
+    [<HttpPut("{id}")>]
+    member this.Put(id:int, [<FromBody>] value:string ) =
+        ()
+
+    [<HttpDelete("{id}")>]
+    member this.Delete(id:int) =
+        ()
